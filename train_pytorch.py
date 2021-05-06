4d56866596a6e01e762858eac4c42d6109065376
Alexandr Maslov <maslov@fastmail.nl>
2021-05-06 15:11:37 +0300

    Update train_pytorch.py

--- train_pytorch.py
+++ train_pytorch.py
@@ -1,4 +1,6 @@
 """
+Author: Alexandr Maslov <maslov314@gmail.com>
+
 IMPORTANT: INSTALL CPU VERSION PYTORCH
 https://pytorch.org/get-started/locally/
 pip install torch==1.7.1+cpu torchvision==0.8.2+cpu torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
