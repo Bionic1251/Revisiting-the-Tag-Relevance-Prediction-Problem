5f8a34cf46b40d9c37fb2a5cff4afb5935e81b28
Alexandr Maslov <maslov@fastmail.nl>
2021-05-06 15:12:37 +0300

    Update train_pytorch_settings.py

--- train_pytorch_settings.py
+++ train_pytorch_settings.py
@@ -1,13 +1,5 @@
-"""
-Those parameters which I tune often
-"""
-
 BATCH_SIZE = 32
 HIDDEN_LAYER_SIZE = 64
 VALIDATION_SPLIT = 0.1
 LEARNING_RATE = 1e-4
 NUM_EPOCHS = 20
-
-# SAVE_PATH_MODEL = 'out_bin/model_final.bin'
-# SAVE_PATH_PERFORMANCE_JSON = './temp/perf0_1.json'
-# SAVE_PATH_PERFORMANCE_PLOT = './img/perf0_1.PNG'
